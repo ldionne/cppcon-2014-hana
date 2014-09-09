2fd9c4e6c3f07465392ad37a66410998ee5660a2
Louis Dionne <ldionne.2@gmail.com>
2014-09-09 10:58:22 -0700

    Add Record example

---
+++ code/record.cpp
@@ -0,0 +1,26 @@
+// Copyright Louis Dionne 2014
+// Distributed under the Boost Software License, Version 1.0.
+
+#include <boost/hana/record/macros.hpp>
+#include <boost/hana/searchable/record_mcd.hpp>
+#include <boost/hana/type.hpp>
+
+#include <cassert>
+#include <string>
+using namespace boost::hana;
+
+
+auto name = decltype_([]{});
+auto age = decltype_([]{});
+
+struct Person {
+    BOOST_HANA_DEFINE_RECORD_INTRUSIVE(Person,
+        ((::name)(std::string)(name))
+        ((::age)(int)(age))
+    );
+};
+
+int main() {
+    Person louis{"Louis", 22};
+    assert(lookup(louis, name) == just("Louis"));
+}
